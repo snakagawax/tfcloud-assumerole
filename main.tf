3ad79be43ab01affc88aa4767a5a8964ae434a21
nakagawa.shota <nakagawa.shota@HL00416.local>
2021-09-02 20:49:29 +0900

    change var

--- main.tf
+++ main.tf
@@ -1,22 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.29.1"
-    }
-  }
-}
-
-variable "account" {}
+variable "aws_account" {}
 
 provider "aws" {
   region = "ap-northeast-1"
   assume_role {
-    role_arn     = "arn:aws:iam::${var.account}:role/multiple-provision-test-role"
+    role_arn     = "arn:aws:iam::${var.aws_account}:role/tfcloud-test-role"
     session_name = "terraform"
   }
 }
 
 resource "aws_s3_bucket" "test" {
-  bucket_prefix = "${var.account}-nakagawa-tfc-test-"
+  bucket_prefix = "${var.aws_account}-tfcloud-test"
 }
