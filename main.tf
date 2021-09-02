30840a57297610ffbc6c201ae094952a86739ecd
nakagawa.shota <nakagawa.shota@HL00416.local>
2021-09-02 20:38:14 +0900

    change var

--- main.tf
+++ main.tf
@@ -9,6 +9,14 @@ terraform {
 
 variable "account" {}
 
+provider "aws" {
+  region = "ap-northeast-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::${var.account}:role/multiple-provision-test-role"
+    session_name = "terraform"
+  }
+}
+
 resource "aws_s3_bucket" "test" {
   bucket_prefix = "${var.account}-nakagawa-tfc-test-"
 }
